86b23c4c7f8361853edd2395bba78884306f4fe1
johnpzsk <johnpesantez2000@gmail.com>
2021-12-04 01:29:31 +0000

    2 files modified

--- src/MedicalTreatmentPanel.js
+++ src/MedicalTreatmentPanel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
 function MedicalTreatmentPanel(){
@@ -21,40 +21,48 @@ function MedicalTreatmentPanel(){
 
   return (
     <div class="mydiv">
-<form class="form-horizontal">
-
   <h1 class="myHeader">
 
     Medical Treatment Panel</h1>    
 
 
+  <div class="mydiv">
+    <label class="lbl">Treat ID</label><br></br>  
+    <input placeholder="treatId"   value={treatId} onChange = {e => setTreatId(e.target.value)}></input>
+  </div>
 
-  <label class="lbl">Treat ID</label>  
-  <input placeholder="treatId"   value={treatId} onChange = {e => setTreatId(e.target.value)}></input>
+  <div class="mydiv">
+    <label class="lbl">Treat Course ID</label><br></br>  
+    <input placeholder="treatCourseId"  value={treatCourseId} onChange = {e => setCourseId(e.target.value)}></input>
+  </div>
 
-  <label class="lbl">Treat Course ID</label>  
-  <input placeholder="treatCourseId"  value={treatCourseId} onChange = {e => setCourseId(e.target.value)}></input>
-
-  <label class="lbl">Type</label>  
+  <div class="mydiv">
+  <label class="lbl">Type</label><br></br>  
   <input placeholder="type"   value={type} onChange = {e => setType(e.target.value)}></input>
+  </div>
 
-  <label class="lbl">Category</label>  
+  <div class="mydiv">
+  <label class="lbl">Category</label><br></br>  
   <input placeholder="category"   value={category} onChange = {e => setCategory(e.target.value)}></input>
+  </div>
 
-  <label class="lbl">Name</label>  
+  <div class="mydiv">
+  <label class="lbl">Name</label><br></br>  
   <input placeholder="name"   value={name} onChange = {e => setName(e.target.value)}></input>
-
-  <label class="lbl">start Date</label>  
-  <input placeholder="startDate"  value={startDate}  onChange = {e => setStartDate(e.target.value)}></input>
+  </div>
   
-   <br/>
-      <button onClick={OnChangeHandler}> SHOW </button>
+  <div class="mydiv">
+  <label class="lbl">start Date</label><br></br>  
+  <input placeholder="startDate"  value={startDate} type="date" onChange = {e => setStartDate(e.target.value)}></input>
+  </div>
+
+  <div class="mydiv">
+      <button class="btn btn-outline-primary " onClick={OnChangeHandler}> SHOW </button>
       <br/>
       <div>
         {items.map((entry)=><li>{entry+"."}</li>)}   
       </div>
-  
-      </form>
+  </div>
     </div>
   );
 
