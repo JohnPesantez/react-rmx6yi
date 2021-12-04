f5a2dcf81885fbc9d48469d3766ebdf1c7535040
johnpzsk <johnpesantez2000@gmail.com>
2021-12-04 02:10:32 +0000

    3 files modified

--- src/App.js
+++ src/App.js
@@ -5,8 +5,35 @@ import MedicalTreatmentPanel from './MedicalTreatmentPanel'
 export default function App() {
 
   return (
-    <div>
-      <MedicalTreatmentPanel />
-    </div>
+
+    
+      <div class="container">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Panel title</h3>
+                    <span class="pull-right">
+                        
+                        <ul class="nav panel-tabs">
+                            <li class="active"><a href="#tab1" data-toggle="tab">Tab 1</a></li>
+                            <li><a href="#tab2" data-toggle="tab">Tab 2</a></li>
+                            <li><a href="#tab3" data-toggle="tab">Tab 3</a></li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="panel-body">
+                    <div class="tab-content">
+                    <div class="tab-pane fade" id="tab1">hello</div>
+                        <div class="tab-pane active" id="tab2">
+                          <MedicalTreatmentPanel/></div>
+                        <div class="tab-pane fade" id="tab3">Hello</div>
+                        </div>
+                </div>
+            </div>
+        </div>
+	</div>
+</div>
+    
   );
 }
