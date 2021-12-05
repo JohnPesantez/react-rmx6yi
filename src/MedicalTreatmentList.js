f3a6bb0c417e6c4bebedc4018cb4453cd9b17ae6
johnpzsk <johnpesantez2000@gmail.com>
2021-12-05 02:27:34 +0000

    1 file added

---
+++ src/MedicalTreatmentList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function MedicalTreatmentList(props){
+  const patientrecords =props.patientJson; 
+  const patients = JSON.parse(patientrecords);
+  const [incategory, setInCategory]=React.useState('');
+  const  listpatientJSON = patients.map((Patient)=>
+  <li key={Patient.treatId}>
+    {Patient.treatId},  
+    {Patient.patientName},  
+    {Patient.treatCourseId}, 
+    {Patient.type},
+    {Patient.category},
+    {Patient.startDate}
+   </li>);
+   const listPatientCategoryJSON = patients.filter(Patient=> Patient.category).map(PatientFiltered=>(<li>{PatientFiltered.category}</li>));  
+  
+    const listPatientByCat= patients.filter(Patient=>Patient.category==incategory).map(Pfiltered=>(
+    <li>
+      {Pfiltered.treatId},  
+      {Pfiltered.patientName},  
+      {Pfiltered.treatCourseId}, 
+      {Pfiltered.type},
+      {Pfiltered.category},
+      {Pfiltered.startDate}
+    </li>));
+    
+return (
+    <div>
+      <h1>Medical Treatment List are:</h1>
+      {listpatientJSON}  
+      <br/><br/>
+      <h2>Medical Treatment List by categories</h2>
+      <input type="text" placeholder="search patient by a category" value={incategory} onChange={(e)=> setInCategory(e.target.value)}/>
+      {listPatientByCat}    
+    </div>
+    
+  );
+
+}
+export default MedicalTreatmentList;
